b2e77a967612f70ef7bebaf6b13f3241b605b88c
masudrana2803 <masudrana2803@gmail.com>
2025-09-07 01:25:41 +0600

    theme

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <h2>I am home</h2>
+    <h2 className='text-brandclr text-center p-10 text-7xl'>I am home</h2>
     </>
   )
 }
